9aa8ac195b2273e0e1a52c56985c202acc00dbb7
Maarten van Keulen <s1173066@windesheim.nl>
2025-11-18 10:09:20 +0100

    change path to forward-slash

--- Vitis/_ide/workspace_journal.py
+++ Vitis/_ide/workspace_journal.py
@@ -7,3 +7,14 @@ client.set_workspace(path="Vitis")
 platform = client.get_component(name="linux")
 status = platform.build()
 
+status = platform.build()
+
+comp = client.get_component(name="sw_to_led")
+comp.build()
+
+status = comp.clean()
+
+status = platform.build()
+
+comp.build()
+
