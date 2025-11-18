51a071e8df4d833e83e144e97a618060b9dc44d3
Maarten van Keulen <s1173066@windesheim.nl>
2025-11-18 10:04:27 +0100

    build platform: linux

--- Vitis/_ide/workspace_journal.py
+++ Vitis/_ide/workspace_journal.py
@@ -1,10 +1,9 @@
-# 2025-11-18T08:55:47.084016
+# 2025-11-18T09:03:38.138937
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="Vitis")
 
-advanced_options = client.create_advanced_options_dict(dt_overlay="0")
-
-platform = client.create_platform_component(name = "linux",hw_design = "$COMPONENT_LOCATION/../bare_minimum_risc-V/hw/RISC_V_wrapper.xsa",os = "standalone",cpu = "microblaze_riscv_0",domain_name = "standalone_microblaze_riscv_0",generate_dtb = False,advanced_options = advanced_options,compiler = "gcc")
+platform = client.get_component(name="linux")
+status = platform.build()
 
---
+++ Vitis/_ide/workspace_journal_12931.py
@@ -0,0 +1,8 @@
+# 2025-11-18T09:00:30.538698
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="Vitis")
+
+vitis.dispose()
+
