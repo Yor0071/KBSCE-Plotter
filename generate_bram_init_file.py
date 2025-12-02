a531a2e0bc839dc35a500103322e794d3cce4257
Maarten van Keulen <s1173066@windesheim.nl>
2025-12-02 20:38:40 +0100

    add initialization data to framebuffer BRAM and a python file to generate such

---
+++ generate_bram_init_file.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+img = Image.open("RCA_Indian_Head_Test_Pattern.svg.png").convert("RGB")
+with open("framebuffer.coe", "w") as f:
+    f.write("memory_initialization_radix=16;\n")
+    f.write("memory_initialization_vector=\n")
+    vals = []
+    for y in range(480):
+        for x in range(640):
+            r,g,b = img.getpixel((x,y))
+            r4 = r >> 4
+            g4 = g >> 4
+            b4 = b >> 4
+            rgb12 = (r4<<8) | (g4<<4) | b4
+            vals.append("{:03X}".format(rgb12))
+    f.write(',\n'.join(vals) + ";\n")
