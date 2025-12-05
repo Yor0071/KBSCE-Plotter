cf50273a5981cc4db0cbdb98f33682e567994212
Maarten van Keulen <s1173066@windesheim.nl>
2025-12-05 11:56:29 +0100

    remove unused external port

--- Vivado/KBSCE-Plotter.gen/sources_1/bd/RISC_V/RISC_V_ooc.xdc
+++ Vivado/KBSCE-Plotter.gen/sources_1/bd/RISC_V/RISC_V_ooc.xdc
@@ -7,7 +7,6 @@
 # of Vivado)
 ################################################################################
 create_clock -name sys_clock -period 10 [get_ports sys_clock]
-create_clock -name BRAM_PORTA_0_clk -period 10 [get_ports BRAM_PORTA_0_clk]
 create_clock -name BRAM_PORTB_0_clk -period 10 [get_ports BRAM_PORTB_0_clk]
 create_clock -name BRAM_PORTA_1_clk -period 10 [get_ports BRAM_PORTA_1_clk]
 
--- Vivado/KBSCE-Plotter.gen/sources_1/bd/RISC_V/hdl/RISC_V_wrapper.vhd
+++ Vivado/KBSCE-Plotter.gen/sources_1/bd/RISC_V/hdl/RISC_V_wrapper.vhd
@@ -2,7 +2,7 @@
 --Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2025.1 (lin64) Build 6140274 Wed May 21 22:58:25 MDT 2025
---Date        : Fri Dec  5 11:30:47 2025
+--Date        : Fri Dec  5 11:52:57 2025
 --Host        : mrt-fed-lap running 64-bit unknown
 --Command     : generate_target RISC_V_wrapper.bd
 --Design      : RISC_V_wrapper
