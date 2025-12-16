deb934b91654f81a114313b1b7597c413fd7cb90
Yor0071 <93204836+Yor0071@users.noreply.github.com>
2025-12-16 14:50:48 +0100

    added homing function

--- Vitis/Plotter/_ide/workspace_journal.py
+++ Vitis/Plotter/_ide/workspace_journal.py
@@ -1,4 +1,4 @@
-# 2025-12-16T11:38:44.305274
+# 2025-12-16T14:25:35.180974800
 import vitis
 
 client = vitis.create_client()
@@ -8,34 +8,6 @@ platform = client.get_component(name="Plotform3")
 status = platform.build()
 
 comp = client.get_component(name="Plot8")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
 comp.build()
 
 status = platform.build()
