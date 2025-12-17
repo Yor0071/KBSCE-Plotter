be4246493b797298b57ef42df3849fbfe4acb554
Maarten van Keulen <s1173066@windesheim.nl>
2025-12-17 14:11:35 +0100

    add new Vitis plotform and app

--- Vitis/Plotter/_ide/workspace_journal.py
+++ Vitis/Plotter/_ide/workspace_journal.py
@@ -1,111 +1,21 @@
-# 2025-12-16T14:25:35.180974800
+# 2025-12-17T13:04:54.862136
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="Plotter")
 
-platform = client.get_component(name="Plotform3")
+advanced_options = client.create_advanced_options_dict(dt_overlay="0")
+
+platform = client.create_platform_component(name = "LinuxPlotform1",hw_design = "$COMPONENT_LOCATION/../../../Vivado/NexysA7_Top.xsa",os = "standalone",cpu = "microblaze_riscv_0",domain_name = "standalone_microblaze_riscv_0",generate_dtb = False,advanced_options = advanced_options,compiler = "gcc")
+
+comp = client.create_app_component(name="Plot9",platform = "$COMPONENT_LOCATION/../LinuxPlotform1/export/LinuxPlotform1/LinuxPlotform1.xpfm",domain = "standalone_microblaze_riscv_0")
+
+platform = client.get_component(name="LinuxPlotform1")
 status = platform.build()
-
-comp = client.get_component(name="Plot8")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
 
 status = platform.build()
 
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-platform = client.get_component(name="Plotform3")
-status = platform.build()
-
-comp = client.get_component(name="Plot8")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
+comp = client.get_component(name="Plot9")
 comp.build()
 
 status = platform.build()
--- Vitis/_ide/workspace_journal.py
+++ Vitis/_ide/workspace_journal.py
@@ -1,8 +1,10 @@
-# 2025-12-11T14:56:58.851672900
+# 2025-12-17T12:53:16.265183
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="Vitis")
 
-vitis.dispose()
+advanced_options = client.create_advanced_options_dict(dt_overlay="0")
+
+platform = client.create_platform_component(name = "linux_plotform1",hw_design = "$COMPONENT_LOCATION/../../Vivado/NexysA7_Top.xsa",os = "standalone",cpu = "microblaze_riscv_0",domain_name = "standalone_microblaze_riscv_0",generate_dtb = False,advanced_options = advanced_options,compiler = "gcc")
 
