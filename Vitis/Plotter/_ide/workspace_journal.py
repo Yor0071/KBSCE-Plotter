3cffe5ae25b808260f3dad9d9cf14549fb0c196c
Yor0071 <93204836+Yor0071@users.noreply.github.com>
2025-12-17 14:57:45 +0100

    Added route algorithm

--- Vitis/Plotter/_ide/workspace_journal.py
+++ Vitis/Plotter/_ide/workspace_journal.py
@@ -1,4 +1,4 @@
-# 2025-12-16T14:25:35.180974800
+# 2025-12-17T14:36:01.304297300
 import vitis
 
 client = vitis.create_client()
@@ -30,85 +30,7 @@ status = platform.build()
 
 comp.build()
 
-status = platform.build()
+vitis.dispose()
 
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-platform = client.get_component(name="Plotform3")
-status = platform.build()
-
-comp = client.get_component(name="Plot8")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
+vitis.dispose()
 
