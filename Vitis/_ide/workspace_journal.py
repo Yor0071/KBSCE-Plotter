7cd61b60e61c2723cb1e7de7ac5cc29f536be67f
Yor0071 <93204836+Yor0071@users.noreply.github.com>
2025-12-18 12:01:13 +0100

    Cleaned Workspace

--- Vitis/_ide/workspace_journal.py
+++ Vitis/_ide/workspace_journal.py
@@ -1,64 +1,8 @@
-# 2025-12-18T10:06:25.632620
+# 2025-12-18T11:47:30.362897300
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="Vitis")
 
-advanced_options = client.create_advanced_options_dict(dt_overlay="0")
-
-platform = client.create_platform_component(name = "lnxPlotformA",hw_design = "$COMPONENT_LOCATION/../../Vivado/NexysA7_Top.xsa",os = "standalone",cpu = "microblaze_riscv_0",domain_name = "standalone_microblaze_riscv_0",generate_dtb = False,advanced_options = advanced_options,compiler = "gcc")
-
-platform = client.get_component(name="lnxPlotformA")
-status = platform.build()
-
-comp = client.create_app_component(name="AppA",platform = "$COMPONENT_LOCATION/../lnxPlotformA/export/lnxPlotformA/lnxPlotformA.xpfm",domain = "standalone_microblaze_riscv_0")
-
-status = platform.build()
-
-comp = client.get_component(name="AppA")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
+vitis.dispose()
 
